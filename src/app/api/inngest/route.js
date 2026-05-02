b633dcf25eb96045defd0c59720f851b5f084b6a
Krishnendu Raut <krishnenduraut2004@gmail.com>
2026-05-02 19:02:37 +0530

    06 Background Job Inngest

---
+++ src/app/api/inngest/route.js
@@ -0,0 +1,17 @@
+// import { serve } from "inngest/next";
+// import { inngest } from "../../../inngest/client";
+
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [],
+// });
+
+
+import { serve } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { processTask } from "../../../inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [processTask],
+});
